7c0a7b13fecd19ddfb21c10ca62de706b70c71d2
muesli <muesli4brekkies@gmail.com>
2024-12-03 01:33:14 +0000

    nicening

--- d2/d2p2/src/d2p2/core.clj
+++ d2/d2p2/src/d2p2/core.clj
@@ -1,35 +1,18 @@
 (ns d2p2.core
   (:gen-class))
-
 (require '[clojure.string :as s])
 
-
-(defn extract [blob]
-  (->>
-   blob
-   (s/split-lines)
-   (map #(s/split %1 #"\s+"))
-   (map #(map read-string %1))))
+(defn extract [blob] (->> blob s/split-lines (map #(s/split %1 #"\s+")) (map #(map read-string %1))))
 
 (defn is-ordered [line] (some #(apply %1 line) [< >]))
 
 (defn deltas-in-range [line]
-  (->>
-   (for [i (-> line (count) (- 1) (range))] (- (nth line i) (nth line (inc i))))
-   (every? #(> 4 (abs %1)))))
+  (every? #(> 4 (abs %1)) (for [i (-> line count (- 1) range)] (- (nth line i) (nth line (inc i))))))
 
-(defn check-opts [line]
-  (->>
-   (reduce #(conj %1 (concat (take %2 line) (drop (inc %2) line))) [] (range (count line)))
-   (some #(and (deltas-in-range %1) (is-ordered %1)))))
+(defn check [line] (and (deltas-in-range line) (is-ordered line)))
 
-(defn validate [line] (-> line (deltas-in-range) (and (is-ordered line)) (or (check-opts line))))
+(defn check-opts [line] (some #(check (concat (take %1 line) (drop (inc %1) line))) (range (count line))))
 
-(defn -main [path]
-  (->>
-   path
-   (slurp)
-   (extract)
-   (filter validate)
-   (count)
-   (println)))
+(defn validate [lines] (filter #(or (check %1) (check-opts %1)) lines))
+
+(defn -main [path] (->> path slurp extract validate count println))
