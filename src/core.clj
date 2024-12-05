39246978691f15a453d7c0f6c15d01b86134ee11
muesli4brekkies <muesli4brekkies@gmail.com>
2024-12-05 00:39:55 +0000

    day 4

--- src/core.clj
+++ src/core.clj
@@ -1,25 +1,22 @@
 (ns core
   (:require
-   [d1.1]
-   [d1.2]
-   [d2.1]
-   [d2.2]
-   [d3.1]
-   [d3.2]))
+   [d1.1] [d1.2]
+   [d2.1] [d2.2]
+   [d3.1] [d3.2]
+   [d4.1] [d4.2]
+   ))
 
-(def inputs
-  ["inputs/d1.input"
-   "inputs/d2.input"
-   "inputs/d3.input"])
-(def fns
-  [[d1.1/solve d1.2/solve]
-   [d2.1/solve d2.2/solve]
-   [d3.1/solve d3.2/solve]])
+(def solvers
+  [["inputs/d1.input" [d1.1/solve d1.2/solve]]
+   ["inputs/d2.input" [d2.1/solve d2.2/solve]]
+   ["inputs/d3.input" [d3.1/solve d3.2/solve]]
+   ["inputs/d4.input" [d4.1/solve d4.2/solve]]])
 
-(defn grab [fn args] (-> args fn read-string (- 1)))
+(defn narg [n args] (-> args n read-string (- 1)))
 
 (defn -main
   [& args]
   (if (> 2 (count args))
     (println "need args yo - day and part eg lein run 1 2")
-    (println ((-> fns (get (grab first args)) (get (grab second args))) (get inputs (grab first args))))))
+    (let [d (get solvers (narg first args)) p (narg second args)]
+      (println ((-> d (get 1) (get p)) (-> d (get 0)))))))
