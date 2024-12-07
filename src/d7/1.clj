c2888e948f7a17b81deaec09f7fbf7bb226836d1
muesli4brekkies <muesli4brekkies@gmail.com>
2024-12-07 22:32:53 +0000

    day 7

--- src/d7/1.clj
+++ src/d7/1.clj
@@ -1,16 +1,16 @@
 (ns d7.1 (:require [clojure.string :as s]))
 
-(defn calc [[t & puz] & ns]
-  (let [x (first puz) [n m] (#(if (nil? %) [0 1] [% %]) (first ns))]
-    (if (empty? puz) (= n t)
-        (when (<= n t)
-          (some #(calc (->> puz (drop 1) (into [t])) %) [(* m x) (+ n x)])))))
+(defn calc [x [t & puz]]
+  (let [n (first puz)]
+    (if (empty? puz)
+      (= x t)
+      (when (<= x t) (some #(calc % (->> puz (drop 1) (into [t]))) [(* (if (zero? x) 1 x) n) (+ x n)])))))
 
 (defn solve [in]
   (->>
    in
    s/split-lines
    (map #(map read-string (s/split % #": | ")))
-   (filter calc)
+   (filter (partial calc 0))
    (map first)
    (apply +)))
--- src/d7/2.clj
+++ src/d7/2.clj
@@ -1,17 +1,7 @@
 (ns d7.2 (:require [clojure.string :as s]))
 
-(defn calc [[t & puz] & ns]
-  (let [x (first puz) [n m c] (#(if (nil? %) [0 1 ""] [% % %]) (first ns))]
-    (if (empty? puz) (= n t)
-        (when (<= n t)
-          (some #(calc (->> puz (drop 1) (into [t])) %) [(read-string (str c x)) 
-                                                         (* m x) (+ n x)])))))
+(defn calc [[t x y & rest]]
+  (if (nil? y) (= x t) (when (<= x t) (some #(calc (conj rest % t)) (map #(% x y) [* + (comp read-string str)])))))
 
 (defn solve [in]
-  (->>
-   in
-   s/split-lines
-   (map #(map read-string (s/split % #": | ")))
-   (filter calc)
-   (map first)
-   (apply +)))
+  (->> in s/split-lines (map #(map read-string (s/split % #": | "))) (reduce (fn [a b] (if (calc b) (+ a (first b)) a)) 0)))
