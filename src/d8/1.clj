c1ca4f81f4c0b157f81bd5af74bb017a82a9e1b6
muesli <muesli4brekkies@gmail.com>
2024-12-09 23:44:03 +0000

    day 8.1

--- src/d8/1.clj
+++ src/d8/1.clj
@@ -2,20 +2,15 @@
   (:require
    [clojure.string :as s]))
 
-(defn walk-line [acc d i f stream]
-  (if (nil? (nth stream (f i d) nil))
-    acc
-    (recur (conj acc (f i d)) d (f i d) f stream)))
-
 (defn find-nodes [nant stream acc ant]
   (let [[a- a+] (sort [ant nant])
-        diff (abs (- nant ant))
-        [i+ i-] [(+ a+ diff) (- a- diff)]
+        d (abs (- nant ant))
+        [i+ i-] [(+ a+ d) (- a- d)]
         [n+ n-] [(nth stream i+ nil) (nth stream i- nil)]]
     (reduce
-     (fn [a [n i f]] (if (nil? n) a (walk-line (conj a i) diff i f stream)))
+     (fn [a [n i]] (if (nil? n) a (conj a i)))
      acc
-     [[n+ i+ +] [n- i- -]])))
+     [[n+ i+] [n- i-]])))
 
 (defn solve [in]
   (let [lines (s/split-lines in)
@@ -25,11 +20,7 @@
         len (count stream)]
     (loop [i 0 res #{} rec {}]
       (if (= i len)
-        [(let [s (reduce (fn [a m] (into a (filter (comp not nil?) (assoc (vec stream) m "*"))))[] res)]
-           (reduce (fn [a i] (str a (get stream i) (if (= i wid) "q" ""))) "" (range (count s))) nil)
-
-          ;)
-         (count res)]
+        (count res)
         (let [c (nth stream i)
               kc (keyword (str c))
               rec? (rec kc)]
@@ -38,9 +29,7 @@
             (if rec?
               (recur
                (inc i)
-               (into res (reduce (partial find-nodes i stream) #{i} rec?))
+               (into res (reduce (partial find-nodes i stream) #{} rec?))
                (assoc rec kc (conj rec? i)))
               (recur (inc i) res (assoc rec kc [i])))))))))
 
-
-(filter nil? [nil nil 1 2 3 nil nil])
