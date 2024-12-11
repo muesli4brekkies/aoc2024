9780a60dc2f1f475d489a69f78539a9fdc7d9ae0
muesli <muesli4brekkies@gmail.com>
2024-12-11 23:16:32 +0000

    day 11.1

--- src/d11/1.clj
+++ src/d11/1.clj
@@ -10,13 +10,5 @@
         (map (fn [n] (->> n (apply str) Integer/parseUnsignedInt str)) (split-at (/ (count nstr) 2) nstr))
         [(str (* 2024 (read-string nstr)))]))))
 
-(defn blink [nums] (flatten (map process nums)))
-
 (defn solve [in]
-  (reduce (fn [res n] [(dec n) (blink res)]) (s/split in #"\s+") (range 25))
-  (let [nums (s/split in #"\s+")]
-    (count
-     (loop [n 25 lnums nums]
-       (if (zero? n)
-         lnums
-         (recur (dec n) (blink lnums)))))))
+  (count (reduce (fn [res _] (flatten (map process res))) (s/split in #"\s+") (range 25))))
