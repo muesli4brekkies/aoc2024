2df705d9855fe0ee2927507659cfc5da63f0a2fc
muesli <muesli4brekkies@gmail.com>
2024-12-11 02:01:50 +0000

    day 10

--- src/d10/1.clj
+++ src/d10/1.clj
@@ -1,29 +1,29 @@
 (ns d10.1)
 
-(def cint {\0 0 \1 1 \2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9})
+(def n# {\0 0 \1 1 \2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9})
 
 (defn next-steps [in wid i]
   (reduce
-   (fn [a j] (let [c (cint (get in j))] (if (=  c (inc (cint (get in i)))) (conj a j) a)))
+   (fn [a j] (let [cur (inc (n# (get in i))) nxt (n# (get in j))] (if (= nxt cur) (conj a j) a)))
    []
-   [(- i wid) (inc i) (dec i) (+ i wid)]))
+   [(inc i) (+ i wid) (- i wid) (dec i)]))
 
-(defn testn [in wid]
-  (fn [res i]
-    (if (not (= \0 (get in i)))
-      res
-      (+ res
-         (loop [stack (list i) seen9 #{} acc 0]
-           (if (empty? stack)
-             acc
-             (let [num (first stack)
-                   pstack (into (next stack) (next-steps in wid num))]
-               (if (= (get in num) \9)
-                 (recur pstack
-                        (conj seen9 num)
-                        (if (some #(= % num) seen9) acc (inc acc)))
-                 (recur pstack seen9 acc)))))))))
+(defn solver [in]
+  (let [wid (inc (count (take-while #(not (= % \newline)) in)))]
+    (reduce
+     (fn [res i]
+       (if (not (= \0 (get in i)))
+         res
+         (into res
+               (set
+                (loop [stack (list i) acc []]
+                  (if (empty? stack)
+                    acc
+                    (let [num (first stack)
+                          stack (into (next stack) (next-steps in wid num))
+                          acc (if (= (get in num) \9) (conj acc num) acc)]
+                      (recur stack acc))))))))
+     []
+     (range (count in)))))
 
-
-(defn solve [in] (let [wid (inc (count (take-while #(not (= % \newline)) in)))]
-                   (reduce (testn in wid) 0 (range (count in)))))
+(defn solve [in] (count (solver in)))
--- src/d10/2.clj
+++ src/d10/2.clj
@@ -1,26 +1,28 @@
 (ns d10.2)
 
-(def cint {\0 0 \1 1 \2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9})
+(def n# {\0 0 \1 1 \2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9})
 
 (defn next-steps [in wid i]
   (reduce
-   (fn [a j] (let [c (cint (get in j))] (if (=  c (inc (cint (get in i)))) (conj a j) a)))
+   (fn [a j] (let [cur (inc (n# (get in i))) nxt (n# (get in j))] (if (= nxt cur) (conj a j) a)))
    []
-   [i (- i wid) (inc i) (dec i) (+ i wid)]))
+   [(inc i) (+ i wid) (- i wid) (dec i)]))
 
-(defn testn [in wid]
-  (fn [res i]
-    (if (not (= \0 (get in i)))
-      res
-      (+ res
-         (loop [stack (list i) acc 0]
-           (if (empty? stack)
-             acc
-             (let [num (first stack)]
-               (recur
-                (into (next stack) (next-steps in wid num))
-                (if (= (get in num) \9) (inc acc) acc)))))))))
+(defn solver [in]
+  (let [wid (inc (count (take-while #(not (= % \newline)) in)))]
+    (reduce
+     (fn [res i]
+       (if (not (= \0 (get in i)))
+         res
+         (into res
+               (loop [stack (list i) acc []]
+                 (if (empty? stack)
+                   acc
+                   (let [num (first stack)
+                         stack (into (next stack) (next-steps in wid num))
+                         acc (if (= (get in num) \9) (conj acc num) acc)]
+                     (recur stack acc)))))))
+     []
+     (range (count in)))))
 
-
-(defn solve [in] (let [wid (inc (count (take-while #(not (= % \newline)) in)))]
-                   (reduce (testn in wid) 0 (range (count in)))))
+(defn solve [in] (count (solver in)))
