f6294df91340feab43c8a263dd4c8ac530d2f781
muesli <muesli4brekkies@gmail.com>
2024-12-16 22:58:06 +0000

    day 16

--- src/d16/1.clj
+++ src/d16/1.clj
@@ -2,43 +2,31 @@
   (:require
    [clojure.string :as s]))
 
-(defn nexts [in i dirs dir seen acc]
+(defn nexts [in i dir wid seen acc]
   (reduce
-   (fn [a ind]
-     (let [nxti (+ i (dirs ind))]
+   (fn [a d]
+     (let [nxti (+ i d)]
        (cond
          (= \# (get in nxti))    a
          (some #(= nxti %) seen) a
          :else (conj a
                      (conj seen i nxti)
-                     (+ acc (if (= ind dir) 1 1001))
+                     (+ acc (if (= d dir) 1 1001))
                      nxti
-                     ind))))
+                     d))))
    '()
-   (keys dirs)))
+   [1 -1 wid (* -1 wid)]))
 
 (defn solve [in]
   (let [wid  (count (take-while #(not (= % \newline)) in))
-        dirs {:n (* -1 wid) :s wid :w -1 :e 1}
         in   (vec (s/replace (s/replace in #"\n" "") #"\." " "))
         scgr (vec (repeat (count in) ##Inf))
         strt (count (take-while #(not (= % \S)) in))]
-
-    (loop [stack (list :e strt 0 []) scgr scgr res ##Inf n 0]
-      (if (empty? stack)
-        res
-        (let [[dir i acc seen] (take 4 stack)
-              adjs (nexts in i dirs dir seen acc)
-              stack (into (drop 4 stack) (if (or (< res acc) (< (get scgr i) acc)) [] (reverse adjs)))]
-          (cond
-            (= \E (get in i))      (recur
-                                    stack
-                                    scgr
-                                    (if (< acc res)
-                                      (do (prn res acc n) acc) res)
-                                    (inc n))
-            :else                  (recur
-                                    stack
-                                    (assoc scgr i acc)
-                                    res
-                                    (inc n))))))))
+    (loop [stack (list 1 strt 0 []) scgr scgr res ##Inf n 0]
+      (if (empty? stack) res
+          (let [[dir i acc seen] (take 4 stack)
+                adjs (nexts in i dir wid seen acc)
+                stack (into (drop 4 stack) (if (or (< res acc) (< (get scgr i) acc)) [] (reverse adjs)))]
+            (if (= \E (get in i))
+              (recur stack scgr (min acc res) (inc n))
+              (recur stack (assoc scgr i acc) res (inc n))))))))
